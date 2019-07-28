752bd2cbd4a1c6b2759bc813528cf58cef481a02
Rohit Kumar <rohitkumar@code-brew.com>
2019-07-28 18:29:25 +0530

    Initial Commit

---
+++ Floaty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Floaty
+//
+//  Created by Rohit Prajapati on 28/07/19.
+//  Copyright © 2019 Rohit Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
